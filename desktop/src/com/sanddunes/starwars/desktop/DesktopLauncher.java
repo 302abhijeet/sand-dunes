82d755786872c061ffc1f4115d3744240e6da3dc
kanishk98 <kanishk.kakar@gmail.com>
2017-11-23 16:23:36 +0530

    Pivoted to 2D game

---
+++ desktop/src/com/sanddunes/starwars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sanddunes.starwars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sanddunes.starwars.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}
